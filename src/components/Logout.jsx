984511a1a30916359e74d7cc617db7fac93bb2ed
Sant99Z <santinod008@gmail.com>
2024-11-03 01:09:10 -0300

    Se agrega la funcionalidad al registro

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,11 +1,12 @@
+// Logout.jsx
 import { useNavigate } from "react-router-dom";
 
 export const Logout = () => {
   const navigate = useNavigate();
 
   const handleLogout = () => {
-    localStorage.removeItem("user"); 
-    navigate("/login");
+    localStorage.removeItem("user"); // Limpia los datos del usuario
+    navigate("/login"); // Redirige a la página de inicio de sesión
   };
 
   return (
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,3 +1,4 @@
+// Register.jsx
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -5,23 +6,22 @@ export const Register = () => {
   const navigate = useNavigate();
   const [error, setError] = useState(false);
 
-  const handleSubmit = async (event) => {
+  const handleRegister = async (event) => {
     event.preventDefault();
-    const name = event.target.elements.name.value;
     const email = event.target.elements.email.value;
     const password = event.target.elements.password.value;
 
-    // Enviar datos del nuevo usuario a la API
+    const newUser = { email, password };
     const response = await fetch('https://6622071827fcd16fa6c8818c.mockapi.io/api/v1/users', {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ name, email, password })
+      body: JSON.stringify(newUser)
     });
 
     if (response.ok) {
-      navigate('/login'); // Redirige al usuario a la página de inicio de sesión
+      navigate('/results'); // Redirige a la página de destinos
     } else {
       setError(true);
     }
@@ -29,24 +29,20 @@ export const Register = () => {
 
   return (
     <main className="register">
-      <form onSubmit={handleSubmit}>
-        <h1>Registro</h1>
-        {error ? <p>Hubo un problema al registrar el usuario</p> : null}
+      <form onSubmit={handleRegister}>
+        <h1>Registrarse</h1>
+        {error && <p>Error en el registro. Intenta de nuevo.</p>}
         <fieldset>
           <label>
-            <span>Nombre</span>
-            <input name="name" type="text" placeholder="John Doe" required />
-          </label>
-          <label>
             <span>Correo</span>
-            <input name="email" type="email" placeholder="johndoe@email.com" required />
+            <input name="email" type="email" placeholder="tucorreo@email.com" required />
           </label>
           <label>
             <span>Contraseña</span>
             <input name="password" type="password" placeholder="*****" required />
           </label>
         </fieldset>
-        <button>Registrarse</button>
+        <button type="submit">Registrarse</button>
       </form>
     </main>
   );
